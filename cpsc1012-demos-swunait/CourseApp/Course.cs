f0b9c6563ab48c587c732827ed0dd8169a01359c
Sam Wu <swu@nait.ca>
2022-12-03 23:14:58 -0700

    Added example of a class with a list for a property

---
+++ cpsc1012-demos-swunait/CourseApp/Course.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace CourseApp
+{
+	public class Course
+	{
+		private string _courseNo;
+		// When you have a property that is a list, you need to create a new list either when you declare the field or in the constructor
+		private List<string> _studentNames = new List<string>();
+
+		// Add method to manage the contents of the list
+		public void AddStudent(string newStudentName)
+		{
+			StudentNames.Add(newStudentName);
+		}
+		public void RemoveStudent(string existingStudentName)
+		{
+			StudentNames.Remove(existingStudentName);
+		}
+		public int StudentCount
+		{
+			get => StudentNames.Count;
+		}
+
+		public string CourseNo
+		{
+			get => _courseNo;
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("CourseNo cannot be blank");
+				}
+				_courseNo = value.Trim();
+			}
+		}
+
+		public List<string> StudentNames
+		{
+			get => _studentNames;
+		}
+ 
+		public Course(string courseNo)
+		{
+			CourseNo = courseNo;
+			// As an alternative to create a new list is in the constructor as follows
+			//_studentNames = new List<string>();
+		}
+	}
+}
+
---
+++ cpsc1012-demos-swunait/CourseApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using CourseApp;
+
+// Create a new Course for CPSC1012
+Course currentCourse = new Course("CPSC1012");
+// Add three students to CPSC1012
+currentCourse.AddStudent("Larry");
+currentCourse.AddStudent("Curly");
+currentCourse.AddStudent("Moe");
+// Print properties of the currentCourse
+PrintCourseInfo(currentCourse);
+
+// Remove Moe from CPSC1012
+currentCourse.RemoveStudent("Moe");
+// Add Shemp to CPSC1012
+currentCourse.AddStudent("Shemp");
+
+// Print properties of the currentCourse
+PrintCourseInfo(currentCourse);
+
+static void PrintCourseInfo(Course currentCourse)
+{
+    Console.WriteLine($"There are {currentCourse.StudentCount} students in {currentCourse.CourseNo}");
+    Console.WriteLine($"The students in {currentCourse.CourseNo} are: ");
+    foreach(string currentStudentName in currentCourse.StudentNames)
+    {
+        Console.WriteLine($"\t{currentStudentName}");
+    }
+}
